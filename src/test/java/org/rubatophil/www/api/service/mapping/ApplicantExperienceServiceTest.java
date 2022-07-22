3f1f0f514c595423463e986d631e892760710adb
AllyHyeseongKim <ally3k@icloud.com>
2022-07-23 05:50:06 +0900

    [feature/apply](#9) Remove the unused libraries
    src/test/java/org/rubatophil/www/api/service/mapping/ApplicantExperienceServiceTest.java

--- src/test/java/org/rubatophil/www/api/service/mapping/ApplicantExperienceServiceTest.java
+++ src/test/java/org/rubatophil/www/api/service/mapping/ApplicantExperienceServiceTest.java
@@ -15,8 +15,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
